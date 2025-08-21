b4d9076d7761e368a001f12c14431c019a4ffff1
AV_Geyer <arbitr705@gmail.com>
2025-08-22 02:30:00 +0300

    feat: make products list private and add add_product method

--- src/category.py
+++ src/category.py
@@ -2,7 +2,6 @@ class Category:
     # Атрибуты класса
     category_count = 0
     product_count = 0
-
     """
     Конструктор класса Category.
     :param name: Название категории
@@ -11,12 +10,33 @@ class Category:
     """
     name: str
     description: str
-    products: list
+    __products: list  # Приватный атрибут
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product):
+        """
+        Добавляет товар в категорию.
+        :param product: Объект класса Product для добавления
+        """
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """
+        Геттер для вывода списка товаров в формате:
+        Название продукта, цена руб. Остаток: количество шт.
+        """
+        products_list = []
+        for product in self.__products:
+            products_list.append(
+                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            )
+        return products_list
